3a3a12076cf02b246b133aa3a230f768499ac9d3
Денис <78109586+marshal894@users.noreply.github.com>
2021-02-10 17:55:56 +0300

    Update DZ3.py

--- DZ3.py
+++ DZ3.py
@@ -55,3 +55,12 @@ def summa ():
     print(f'Финальная сумма  {summa_res}')
 summa()
 # Задание № 6
+def int_func(word):
+    latin_char = 'qwertyuiopasdfghjklzxcvbnm'
+    return word.title() if not set(word).difference(latin_char) else False
+
+words = input('Введите строку из слов разделенных пробелами ').split()
+for w in words:
+    result = int_func(w)
+    if result:
+        print(int_func(w), ' ')
