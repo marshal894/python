bd9c32c84d18bdcfdbd3029b3706d24cb83f855c
Денис <78109586+marshal894@users.noreply.github.com>
2021-02-10 17:57:23 +0300

    Update DZ3.py

--- DZ3.py
+++ DZ3.py
@@ -58,7 +58,6 @@ summa()
 def int_func(word):
     latin_char = 'qwertyuiopasdfghjklzxcvbnm'
     return word.title() if not set(word).difference(latin_char) else False
-
 words = input('Введите строку из слов разделенных пробелами ').split()
 for w in words:
     result = int_func(w)
